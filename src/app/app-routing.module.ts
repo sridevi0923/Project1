ea659590149c257da7427b85a421be94daf1826c
anitha0203 <anithab@visheshcountrycache.com>
2022-02-26 10:08:22 +0530

    test

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  path:'',Component:
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
